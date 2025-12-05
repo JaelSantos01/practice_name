09dcb9aae01f11fa9b99a3dff60d96be4896236d
JaelSantos01 <20223tn110@utez.edu.mx>
2025-12-04 18:07:37 -0600

    Cambio de api_base

--- client/src/models/api.controller.js
+++ client/src/models/api.controller.js
@@ -1,6 +1,6 @@
 const ApiController = {};
 const ENV = import.meta.env;
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}/api/nom`;
+const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
 
 ApiController.namesAll = async names => {
     try {
